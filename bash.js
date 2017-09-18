b5820590b7cf54ddd4105d0224614b6cbd9e244c
KeriPMiller <KeriPMiller@users.noreply.github.com>
2017-09-18 11:46:22 -0400

    added ls function

--- bash.js
+++ bash.js
@@ -1,10 +1,13 @@
-var commands = require('./commands')
+var commands = require('./commands');
+
+
+var userCommand = 'pwd'
 
 process.stdout.write('prompt > ')
 
+
 process.stdin.on('data', function(data){
+  userCommand =  data.toString().trim();
+  commands[userCommand]();
 
 })
-var userCommand = data.toString().trim();
-
-commands[userCommand]();
--- commands.js
+++ commands.js
@@ -1,10 +1,23 @@
+var fs = require('fs');
+
 module.exports = {
-  pwd: function(){
+  pwd: function() {
     process.stdout.write(process.mainModule.filename)
     process.stdout.write('\nprompt > ')
-  }
-  date: function(){
+  },
+  date: function() {
     process.stdout.write(Date())
     process.stdout.write('\nprompt > ')
+  },
+  ls: function() {
+    fs.readdir('.', function(err, files) {
+      if (err) {
+        throw err;
+      }
+      files.forEach(function(file) {
+        process.stdout.write(file.toString() + "\n")
+      })
+      process.stdout.write('prompt > ');
+    })
   }
 }
